9a645dce9c1496dba5501a3a0058db9e32e9cd89
yehiarashed24-maker <yehia.rashed24@icloud.com>
2026-06-23 01:22:49 +0300

    Add files via upload

--- nourgine site/script.js
+++ nourgine site/script.js
@@ -47,7 +47,7 @@ async function sendMessage() {
   chatBox.scrollTop = chatBox.scrollHeight;
 
   try {
-    const response = await fetch("/.netlify/functions/chat", {
+    const response = await fetch("/api/chat",  {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
